7d6d24649b43ee56635c4d72af70e28cbfb60047
Miroslava Tsaneva <156629879+gothbanshee@users.noreply.github.com>
2024-01-29 01:14:58 +0200

    MidExam 05

---
+++ MidExam.05/01.BonusScoringSystem/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.BonusScoringSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int students = int.Parse(Console.ReadLine());
+            double lectures = int.Parse(Console.ReadLine());
+            int bonus = int.Parse(Console.ReadLine());
+
+            double max = 0;
+            double maxAt = 0;
+
+            for (int i = 0; i < students; i++)
+            {
+                double attends = int.Parse(Console.ReadLine());
+                double total = Math.Ceiling((attends / lectures) * (5 + bonus));
+
+                if (total > max)
+                {
+                    max = total;
+                    maxAt = attends;
+                }
+            }
+            Console.WriteLine($"Max Bonus: {max}.");
+            Console.WriteLine($"The student has attended {maxAt} lectures.");
+        }
+    }
+}
---
+++ MidExam.05/02.MuOnline/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading;
+
+namespace _02.MuOnline
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rooms = Console.ReadLine()
+                .Split("|");
+
+            int health = 100;
+            int bitcoins = 0;
+
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                string[] command = rooms[i].Split();
+
+                if (command[0] == "potion")
+                {
+                    health += int.Parse(command[1]);
+                    if (health > 100)
+                    {
+                        Console.WriteLine($"You healed for {100 - (health - int.Parse(command[1]))} hp.");
+                        health = 100;
+                        Console.WriteLine($"Current health: {health} hp.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You healed for {command[1]} hp.");
+                        Console.WriteLine($"Current health: {health} hp.");
+                    }
+                }
+                else if (command[0] == "chest")
+                {
+                    bitcoins += int.Parse(command[1]);
+                    Console.WriteLine($"You found {command[1]} bitcoins.");
+                }
+                else
+                {
+                    health -= int.Parse(command[1]);
+                    if (health > 0)
+                    {
+                        Console.WriteLine($"You slayed {command[0]}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You died! Killed by {command[0]}.");
+                        Console.WriteLine($"Best room: {i + 1}");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine("You've made it!");
+            Console.WriteLine($"Bitcoins: {bitcoins}");
+            Console.WriteLine($"Health: {health}");
+        }
+    }
+}
